89f8f589a14415e9937ff253503d38d19b22561b
yasharp <yasharp1383@gmail.com>
2022-12-04 20:47:31 +0330

    Revert "?"
    This reverts commit 3fb6f3065c51633ac9bdba955677dbaa8e152418.

--- power.c
+++ power.c
@@ -1,12 +1,19 @@
 //a^b 
-// O(b) version
+// O(log(b))
 #include <stdio.h>
+
+long long poww(long long a , long long b){
+    if (!b)
+        return 1 ; 
+    long long x = poww(a , b/2) ; 
+    //if (b%2)
+    //    return x*x*a ; 
+    return x*x ; 
+}
 
 
 int main(){
-    long long a , b , ans = 1 ; 
+    long long a , b ; 
     scanf("%lld %lld" , &a , &b) ; 
-    for (int i = 1 ; i <= b ; i++)
-        ans = ans * a ; 
-    printf("%lld" , ans) ; 
+    printf("%lld" , poww(a , b)) ; 
 }
