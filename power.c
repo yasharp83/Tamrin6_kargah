c8781319a595c0d448b37e45ee67554455eff114
yasharp <yasharp1383@gmail.com>
2022-12-04 20:55:48 +0330

    Revert "making bug in code"
    This reverts commit d907acac253266760c50730a6c0b8e9d4471a24c.

--- power.c
+++ power.c
@@ -6,8 +6,8 @@ long long poww(long long a , long long b){
     if (!b)
         return 1 ; 
     long long x = poww(a , b/2) ; 
-    //if (b%2)
-    //    return x*x*a ; 
+    if (b%2)
+        return x*x*a ; 
     return x*x ; 
 }
 
